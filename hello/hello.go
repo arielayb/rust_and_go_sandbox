7a53df434a156128db4b7a26948f8cb13afce50a
arielayb <aybar.ariel@gmail.com>
2024-11-12 20:12:41 -0700

    updated files

--- hello/hello.go
+++ hello/hello.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 	"rsc.io/quote"
 )
 
 func main() {
+	// Set properties of the predefined Logger, including
+	// the log entry prefix and a flag to disable printing
+	// the time, source file, and line number.
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
 	fmt.Println(quote.Go())
 
 	// Get a greeting message and print it.
-	message := greetings.Hello("Gladys")
+	message, err := greetings.Hello("")
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(message)
 }
