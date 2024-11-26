df3a4412c495035f5c74eb373c8558450eeca436
Steam Deck User <deck@steamdeck.hsd1.co.comcast.net>
2024-11-25 23:23:10 -0700

    added websocket WIP

---
+++ rabbitmq/receive.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+  "log"
+
+  amqp "github.com/rabbitmq/amqp091-go"
+)
+
+func failOnError(err error, msg string) {
+  if err != nil {
+    log.Panicf("%s: %s", msg, err)
+  }
+}
+
+
